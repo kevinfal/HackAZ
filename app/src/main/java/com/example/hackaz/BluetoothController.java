abc2308f134de79c991597528db8d5174a7f8e2f
Maksim Gusev <mgusev@email.arizona.edu>
2020-01-17 23:11:38 -0700

    Upd1 - Layout XML

--- app/src/main/java/com/example/hackaz/BluetoothController.java
+++ app/src/main/java/com/example/hackaz/BluetoothController.java
@@ -3,10 +3,23 @@ package com.example.hackaz;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 
-public class BluetoothController<bluetoothAdapter> {
+import static android.bluetooth.BluetoothAdapter.ACTION_REQUEST_ENABLE;
+import static android.support.v4.app.ActivityCompat.startActivityForResult;
+
+public class BluetoothController {
+
+    //---> ATTRIBUTES <---//
 
 
 
+    //---> GETTERS/SETTERS <---//
+
+
+
+    //---> OTHER FUNCTIONS <---//
+
+
+    // Setting up Bluetooth in a given device to use
     public void setUpBluetooth() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
@@ -14,9 +27,14 @@ public class BluetoothController<bluetoothAdapter> {
             System.out.println("Failure to connect. Bluetooth not supported on device.");
         }
 
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        //if (!bluetoothAdapter.isEnabled()) {
+        //    Intent enableBtIntent = new Intent(ACTION_REQUEST_ENABLE);
+        //    startActivityForResult(enableBtIntent, ACTION_REQUEST_ENABLE);
+        //}
+
+        if (!bluetoothAdapter.isEnabled()){
+            bluetoothAdapter.enable();
         }
+
     }
 }
